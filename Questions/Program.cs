1acdf96fb84af987625a3567d6c2d42dec28122f
Thundercats <prasad.haze@gmail.com>
2018-02-14 23:41:36 -0800

    * leetcode.sln: Init solution
    * README.md:
    * Program.cs:
    * questions.csproj:

---
+++ Questions/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Questions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+Implement leetcode questions as much in C# 
