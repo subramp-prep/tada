baaaca77042c51e6cf7543ec1724952dcf0392d7
Thundercats <prasad.haze@gmail.com>
2018-02-18 11:43:34 -0800

    * leetcode.sln: Adding an optimal solution to validPalindrome,. This   was accepted! Adding solution to 3Sum, this was not accepted. Since it returns   duplicate array
    * leetcode.userprefs:
    * 15-3Sum.cs:
    * 125-ValidPalindrome.cs:

---
+++ Questions/125-ValidPalindrome.cs
@@ -0,0 +1,75 @@
+﻿using System;
+namespace questions
+{
+    public class ValidPalindrome
+    {
+        /// <summary>
+        /// https://leetcode.com/problems/valid-palindrome/description/
+        /// Perform was not accepted, since the time complexity was too high for 475th of 476 input
+        /// </summary>
+        /// <returns>The peform.</returns>
+        /// <param name="content">Content.</param>
+
+        public static bool Peform(string content)
+        {
+            string filteredString = FilteredString(content);
+            for (int lIndex = 0, hIndex = filteredString.Length - 1; lIndex < filteredString.Length / 2; 
+                 lIndex++, hIndex--)
+            {
+                if (filteredString[lIndex] != filteredString[hIndex])
+                    return false;
+            }
+
+            return true;
+        }
+
+        public static string FilteredString(string s)
+        {
+            string filteredString = "";
+
+            for (int index = 0; index < s.Length; index++)
+            {
+                if (char.IsLetterOrDigit(s[index]))
+                {
+                    filteredString += s[index];
+                }
+            }
+            return filteredString.ToLower();
+        }
+
+
+        /// <summary>
+        /// This is an optimal solution. The submission was accepted!
+        /// </summary>
+        /// <returns><c>true</c>, if optimal was performed, <c>false</c> otherwise.</returns>
+        /// <param name="content">Content.</param>
+        public bool PerformOptimal(string content)
+        {
+            int lIndex = 0;
+            int hIndex = content.Length - 1;
+            while (lIndex < hIndex)
+            {
+                if (!char.IsLetterOrDigit(content[lIndex]))
+                {
+                    lIndex++;
+                }
+                else if (!char.IsLetterOrDigit(content[hIndex]))
+                {
+                    hIndex--;
+                }
+                else
+                {
+                    if (char.ToLower(content[lIndex]) != char.ToLower(content[hIndex]))
+                    {
+                        return false;
+                    }
+
+                    lIndex++;
+                    hIndex--;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Questions/15-3Sum.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace questions
+{
+    /// <summary>
+    /// Problem URL: https://leetcode.com/problems/3sum/description/
+    /// </summary>
+    public class Sum
+    {
+        public static IList<IList<int>> ThreeSum(int[] nums)
+        {
+            HashSet<int> hashInt = new HashSet<int>(nums);
+            int sum = 0;
+
+            IList<IList<int>> result = new List<IList<int>>();
+
+            Array.Sort(nums);
+
+            for (int index1 = 0, index2 = 1; index2 < nums.Length; index1++, index2++)
+            {
+                if (index1 > 0 && nums[index2] == nums[index1])
+                {
+                    continue;
+                }
+
+                int missing = sum - (nums[index1] + nums[index2]);
+                if(hashInt.Contains(missing))
+                {
+                    IList<int> threeSum = new List<int>();
+
+                    threeSum.Add(nums[index1]);
+                    threeSum.Add(nums[index2]);
+                    threeSum.Add(missing);
+
+                    result.Add(threeSum);
+                }
+             }
+             return result;
+        }
+
+        /// <summary>
+        /// Froms the wiki: https://en.wikipedia.org/wiki/3SUM
+        /// </summary>
+        public static void fromWiki()
+        {}
+    }
+
+
+
+}
