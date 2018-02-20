b51b23dc7f14ad92c994080e1f8b9a5f195af7a7
subramp-prep <prasad.haze@gmail.com>
2018-02-19 21:56:55 -0800

    Update string permutation with logic to dedupe string with repeated characters

--- Questions/Concepts/Backtracking-StringPermutations.cs
+++ Questions/Concepts/Backtracking-StringPermutations.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace questions
 {
     public class Backtracking_StringPermutations
@@ -11,7 +13,9 @@ namespace questions
 
         public static void Permute(string s)
         {
-            PermuteHelper(s, "");
+            HashSet<string> set = new HashSet<string>();
+            
+            PermuteHelper(s, "", set);
         }
 
         private static void PermuteHelper(string s, string chosen)
@@ -41,5 +45,37 @@ namespace questions
                 }
             }
         }
+
+		private static void PermuteHelper(string s, string chosen, HashSet<string> set)
+		{
+			if (s.Length == 0)
+			{
+                if (!set.Contains(chosen))
+                {
+                    set.Add(chosen);
+                    Console.WriteLine(chosen);
+                }
+			}
+			else
+			{
+				// Step1 Choose
+				// Step2 Explore
+				// Step3 Unchoose
+				for (int index = 0; index < s.Length; index++)
+				{
+					// chooose
+					char c = s[index];
+					chosen += c;
+					s = s.Remove(index, 1);
+
+					// explore
+					PermuteHelper(s, chosen);
+
+					//unchoose
+					s = s.Insert(index, c.ToString());
+					chosen = chosen.Remove(chosen.Length - 1);
+				}
+			}
+		}
     }
 }
