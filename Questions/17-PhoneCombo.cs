4f014fcbb4b0b10189d84e86fd0bba226f1ff943
subramp-prep <prasad.haze@gmail.com>
2018-02-20 20:47:08 -0800

    * leetcode.userprefs: Initial checkin for phone number combination

---
+++ Questions/17-PhoneCombo.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace questions
+{
+    public class PhoneCombo
+    {
+        /// <summary>
+        /// Question URL: https://leetcode.com/problems/letter-combinations-of-a-phone-number/description/
+        /// </summary>
+
+        private static string[] representation = {
+            "",
+            "",
+            "abc",
+            "def",
+            "ghi",
+            "jkl",
+            "mno",
+            "pqrs",
+            "tuv",
+            "wxyz"
+        };
+
+        public static IList<string> LetterCombinations(string digits)
+        {
+            string[] digitRepresentation = new string[digits.Length];
+            for (int index = 0; index < digits.Length; index++)
+            {
+                digitRepresentation[index] = representation[digits[index] - '0'];
+            }
+
+            return Perform(digitRepresentation);
+        }
+
+        private static IList<string> Perform(string[] digits)
+        {
+            List<string> combinations = new List<string>();
+
+            PerformHelper(digits, "", combinations);
+
+            return combinations;
+        }
+
+        private static void PerformHelper(string[] digits, string combo, List<string> combinations)
+        {
+            if (combo.Length == digits.Length)
+            {
+                combinations.Add(combo);
+            }
+            else
+            {
+                // choose -> explore -> unchoose
+            }
+        }
+    }
+}
