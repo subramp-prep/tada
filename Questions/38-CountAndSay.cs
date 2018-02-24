4df2a08191ef314a06e12957123a39136c570566
subramp-prep <prasad.haze@gmail.com>
2018-02-23 19:40:52 -0800

    Checking in number permutations

---
+++ Questions/38-CountAndSay.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace questions
+{
+	/// <summary>
+	/// Question URL https://leetcode.com/problems/count-and-say/description/
+	/// </summary>
+	public class CountAndSay
+    {
+        /// <summary>
+        /// Note: Didnt go well!!! 
+        /// Question was not really understandable
+        /// </summary>
+        /// <returns>The perform.</returns>
+        /// <param name="n">N.</param>
+
+        public static string  Perform(int n)
+		{
+            int[] counter = new int[10];
+            while (n > 0)
+            {
+                int mod = n % 10;
+                counter[mod] += 1;
+
+                n /= 10;
+            }
+
+            string output = string.Empty;
+            for (int index = 9; index >=0; index--)
+            {
+                if (counter[index] > 0)
+                {
+                    output += string.Format("{0}{1}", counter[index], index);
+                }
+            }
+
+            return output;
+		}
+    }
+}
---
+++ Questions/48-Permutations.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace questions
+{
+    public class Permutations
+    {
+        public static IList<IList<int>> Permute(int[] nums)
+        {
+            IList<IList<int>> output = new List<IList<int>>();
+
+            List<int> chosen = new List<int>();
+            PermuteHelper(nums.ToList(), chosen, output);
+            return output;
+        }
+
+        private static void PermuteHelper(List<int> input, List<int> chosen, IList<IList<int>> output)
+        {
+            if (input.Count == 0)
+            {
+                output.Add(chosen.ToArray());
+            }
+            else
+            {
+                for (int index = 0; index < input.Count(); index++)
+                {
+                    //choose
+                    int c = input[index];
+                    chosen.Add(c);
+                    input.Remove(input[index]);
+
+                    //explore
+                    PermuteHelper(input, chosen, output);
+
+                    //unchoose
+                    chosen.RemoveAt(chosen.Count() - 1);
+                    input.Insert(index, c);
+                }
+            }
+        }
+    }
+}
