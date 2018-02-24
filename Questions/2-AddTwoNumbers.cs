add92e8ac902d27a735a179f4aed74f0fb5116f3
subramp-prep <prasad.haze@gmail.com>
2018-02-23 20:20:17 -0800

    Addition of 2 numbers from a linked list

---
+++ Questions/2-AddTwoNumbers.cs
@@ -0,0 +1,53 @@
+﻿using System;
+namespace questions
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x) { val = x; }
+    }
+
+    public class AddTwoNumbers
+    {
+		/// <summary>
+		/// Question URL : https://leetcode.com/problems/add-two-numbers/description/
+		/// </summary>
+		/// <returns>The perform.</returns>
+		/// <param name="l1">L1.</param>
+		/// <param name="l2">L2.</param>
+		public ListNode Perform(ListNode l1, ListNode l2)
+        {
+            ListNode head = new ListNode(0);
+            ListNode result = head;
+            int carry = 0;
+
+            while (l1 != null || l2 != null || carry > 0)
+            {
+                int n1 = 0;
+                int n2 = 0;
+                int sum = 0;
+
+                if (l1 != null)
+                {
+                    n1 = l1.val;
+                    l1 = l1.next;
+                }
+
+                if (l2 != null)
+                {
+                    n2 = l2.val;
+                    l2 = l2.next;
+                }
+
+                sum = n1 + n2 + carry;
+                carry = sum / 10;
+
+                result.next = new ListNode(sum % 10);
+                result = result.next;
+            }
+
+            return head.next;
+        }
+    }
+}
