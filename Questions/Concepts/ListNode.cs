7e7b307bce54739eb3d0acd151659351a0f993fc
subramp-prep <prasad.haze@gmail.com>
2018-02-25 12:50:15 -0800

    Reverse an LL

---
+++ Questions/Concepts/ListNode.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace questions
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x) { val = x; }
+
+
+        public static ListNode AddNodeToTail(ListNode head, ListNode node)
+        {
+            if (head == null)
+            {
+                return node;
+            }
+
+            ListNode cache = null;
+            ListNode temp = head;
+
+            while (temp != null)
+            {
+                cache = temp;
+                temp = temp.next;
+            }
+            cache.next = node;
+
+            return head;
+        }
+
+        public static ListNode Reverse(ListNode head)
+        {
+            ListNode result = null;
+            while (head != null)
+            {
+                ListNode next = head.next;  // cache next
+                head.next = result;         // assign next
+                result = head;              // cache head
+                head = next;                // assign head
+            }
+
+            return result;
+        }
+    }
+}
