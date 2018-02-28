7a489e63564403df7e430af5af635687d4616a30
subramp-prep <prasad.haze@gmail.com>
2018-02-28 01:32:37 -0800

    Resolving Q100 - same tree

---
+++ Questions/100-SameTree.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace questions
+{
+    /// <summary>
+    /// Question URL: https://leetcode.com/problems/same-tree/description/
+    /// Given two binary trees, write a function to check if they are the same or not.
+    /// Two binary trees are considered the same if they are structurally identical and the nodes have the same value.
+    /// </summary>
+
+    public class SameTree
+    {
+        public static bool IsSameTree(TreeNode p, TreeNode q)
+        {
+            if (p == null && q == null) return true;
+
+            if (p == null || q == null) return false;
+
+            if (p.val == q.val)
+            {
+                return IsSameTree(p.left, q.left) && IsSameTree(p.right, q.right);
+            }
+            
+            return false;
+        }
+    }
+}
---
+++ Questions/Concepts/TreeNode.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace questions
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) { val = x; }
+    }
+}
