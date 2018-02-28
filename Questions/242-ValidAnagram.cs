cba26f461b524cc1a78183234543447ced26150e
subramp-prep <prasad.haze@gmail.com>
2018-02-28 01:10:58 -0800

    Submitting Q242 Q266

---
+++ Questions/242-ValidAnagram.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace questions
+{
+    /// <summary>
+    /// Question URL: https://leetcode.com/problems/valid-anagram/description/
+    /// 
+    /// Given two strings s and t, write a function to determine if t is an anagram of s.
+    //    For example,
+    //    s = "anagram", t = "nagaram", return true.
+    //    s = "rat", t = "car", return false.
+    //Note:
+    //You may assume the string contains only lowercase alphabets.
+    //Follow up:
+    //What if the inputs contain unicode characters? How would you adapt your solution to such case?
+    /// </summary>
+
+    public class ValidAnagram
+    {
+        public static bool IsAnagram(string s, string t)
+        {
+            if (s.Length != t.Length)
+            {
+                return false;
+            }
+
+            Dictionary<char, int> validator = new Dictionary<char, int>();
+            foreach (var elem in s)
+            {
+                int counter;
+                if(!validator.TryGetValue(elem, out counter))
+                {
+                    validator.Add(elem, 1);
+                }
+                else
+                {
+                    validator[elem] += 1;
+                }
+            }
+
+            foreach (var elem in t)
+            {
+                int reducer;
+                if(validator.TryGetValue(elem, out reducer))
+                {
+                    reducer--;
+
+                    if (reducer <= 0)
+                    {
+                        validator.Remove(elem);
+                    }
+                    else
+                    {
+                        validator[elem] = reducer;
+                    }
+                }
+            }
+
+            return (validator.Count == 0);
+        }
+    }
+}
---
+++ Questions/266-PalindromePermutation.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace questions
+{
+    /// <summary>
+    /// Question URL: https://leetcode.com/problems/palindrome-permutation/description/
+    /// Given a string, determine if a permutation of the string could form a palindrome.
+    /// For example,
+    //  "code" -> False, "aab" -> True, "carerac" -> True.
+    /// </summary>
+
+    public class PalindromePermutation
+    {
+        public static bool CanPermutePalindrome(string s)
+        {
+            bool isOddLength = (s.Length % 2 == 1);
+            bool isEvenLength = (s.Length % 2 == 0);
+
+            HashSet<char> checker = new HashSet<char>();
+            foreach (var c in s)
+            {
+                if (checker.Contains(c))
+                {
+                    checker.Remove(c);
+                }
+                else
+                {
+                    checker.Add(c);
+                }
+            }
+
+            if (isOddLength && checker.Count == 1 ||
+                isEvenLength && checker.Count == 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
