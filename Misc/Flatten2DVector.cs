02f94e9fafb1573e38f641b24aeb46e24a54d690
Thundercats <prasad.haze@gmail.com>
2018-03-15 00:03:14 -0700

    Completing 2D vector problem

--- Misc/Flatten2DVector.cs
+++ Misc/Flatten2DVector.cs
@@ -8,7 +8,6 @@ namespace Misc
         //Implement an iterator to flatten a 2d vector.
         //For example,
         //Given 2d vector =
-
         //[
         //  [1,2],
         //  [3],
@@ -16,20 +15,19 @@ namespace Misc
         //]
         //By calling next repeatedly until hasNext returns false, 
         //the order of elements returned by next should be: [1,2,3,4,5,6].
-
         //Follow up:
         //As an added challenge, try to code it using only 
         //iterators in C++ or iterators in Java.
-
         /**
          * Your Vector2D will be called like this:
          * Vector2D i = new Vector2D(vec2d);
          * while (i.HasNext()) v[f()] = i.Next();
          */
 
-        IList<IList<int>> _vec2d;
-        int _x;
-        int _y;
+        private IList<IList<int>> _vec2d;
+        private int _x;
+        private int _y;
+        private int _elementCount;
 
         public Vector2D(IList<IList<int>> vec2d)
         {
@@ -37,35 +35,38 @@ namespace Misc
             _x = 0;
             _y = 0;
 
-            Console.WriteLine(_vec2d.Count);
-            Console.WriteLine(_vec2d[_x].Count);
+            _elementCount = 0;
+            foreach (var x in vec2d)
+            {
+                for (int i = 0; i < x.Count; i++)
+                {
+                    _elementCount++;
+                }
+            }
         }
+
 
         public bool HasNext()
         {
-            return _x <= _vec2d.Count &&
-                   _y <= _vec2d[_x].Count;
+            return _elementCount-- > 0;
         }
 
         public int Next()
         {
-            //foreach (var elem in _vec2d)
-            //{
-            //    foreach (var e in elem)
-            //    {
-
-            //    }
-            //}
-
             int element = 0;
-            for (; _x < _vec2d.Count; _x++)
+            if (_y < _vec2d[_x].Count)
             {
-                for (; _y < _vec2d[_x].Count; _y++)
+                element = _vec2d[_x][_y++];
+            }
+            else
+            {
+                _y = 0; // reset
+                _x++;
+
+                if (_x < _vec2d.Count)
                 {
-                    element = _vec2d[_x][_y];
-                    break;
+                    element = _vec2d[_x][_y++];
                 }
-                break;
             }
 
             return element;
