38dd43a73ff5709b897d35e3633f8f9a227fb7a8
subramp-prep <prasad.haze@gmail.com>
2018-03-17 15:53:26 -0700

    Selected few questions pending implementation

--- Questions/209-MinSizeSubarray.cs
+++ Questions/209-MinSizeSubarray.cs
@@ -1,10 +1,29 @@
 ﻿using System;
-namespace Questions
+using System.Collections.Generic;
+
+namespace questions
 {
-    public class _09_MinSizeSubarray
+    public class MinSizeSubarray
     {
-        public _09_MinSizeSubarray()
+        // Question URL: https://leetcode.com/problems/minimum-size-subarray-sum/description/
+        //Given an array of n positive integers and a positive integer s, 
+        //find the minimal length of a contiguous subarray of which the sum ≥ s.
+        //If there isn't one, return 0 instead.
+
+        //For example, given the array[2, 3, 1, 2, 4, 3] and s = 7,
+        //the subarray[4, 3] has the minimal length under the problem constraint.
+
+        //click to show more practice.
+
+        //More practice:
+        //If you have figured out the O(n) solution, 
+        //try coding another solution of which the time complexity is O(n log n).
+        public static int MinSubArrayLen(int s, int[] nums)
         {
+            int minSize = 0;
+            // TODO pending implementation
+
+            return minSize;
         }
     }
 }
--- Questions/448-DisappearedNumbersInArray.cs
+++ Questions/448-DisappearedNumbersInArray.cs
@@ -1,10 +1,29 @@
 ﻿using System;
-namespace Questions
+using System.Collections.Generic;
+
+namespace questions
 {
-    public class _48_DisappearedNumbersInArray
+    // Question URL: 
+    //https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/description/
+    //Given an array of integers where 1 ≤ a[i] ≤ n(n = size of array), 
+    //some elements appear twice and others appear once.
+    //Find all the elements of [1, n] inclusive that do not appear in this array.
+    //Could you do it without extra space and in O(n) runtime? 
+    //You may assume the returned list does not count as extra space.
+    //Example:
+    //Input:
+    //[4,3,2,7,8,2,3,1]
+    //Output:
+    //[5,6]
+    public class DisappearedNumbersInArray
     {
-        public _48_DisappearedNumbersInArray()
+        public static IList<int> FindDisappearedNumbers(int[] nums)
         {
+            IList<int> missingNumbers = new List<int>();
+
+            // TODO Pending implementation
+
+            return missingNumbers;
         }
     }
 }
